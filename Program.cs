95d7cbebc9bc2f673c194310ab521e9e1e45bc96
skullkon <skullkon@gmail.com>
2021-12-21 22:17:58 +0700

    thread making

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+            
+            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork2));
+            thread2.Start(int.MaxValue);
+        }
+        
+        static void DoWork()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+
+                if (j % 100 == 0)
+                {
+                    Console.WriteLine("DoWork");
+                }
+            }
+        }
+        
+        static void DoWork2(object max)
+        {
+
+            int j = 0;
+            for (int i = 0; i < (int)max; i++)
+            {
+                j++;
+                
+                if (j % 100 == 0)
+                {
+                    Console.WriteLine("DoWork   2");
+                }
+            }
+        }
+    }
+}
+
