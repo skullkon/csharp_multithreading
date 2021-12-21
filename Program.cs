b94af91cb7ca182a04504a83390c17b5b3d6b5f8
skullkon <skullkon@gmail.com>
2021-12-21 23:09:45 +0700

    async / await

--- Program.cs
+++ Program.cs
@@ -5,19 +5,43 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        public static object locker = new object();
         static void Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(DoWork));
-            thread.Start();
+            // Thread thread = new Thread(new ThreadStart(DoWork));
+            // thread.Start();
+            //
+            // Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork2));
+            // thread2.Start(int.MaxValue);
+            // DoWorkAsyc();
+            //
+            // int j = 0;
+            // for (int i = 0; i < 10000; i++)
+            // {
+            //     j++;
+            //
+            //     if (j % 100 == 0)
+            //     {
+            //         Console.WriteLine("Main");
+            //     }
+            // }
+            //
+            // Console.ReadLine();
             
-            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork2));
-            thread2.Start(int.MaxValue);
+            
+        }
+
+        static async Task DoWorkAsyc()
+        {
+            await Task.Run(() => DoWork());
+            Console.WriteLine("Do work async");
         }
         
         static void DoWork()
         {
             int j = 0;
-            for (int i = 0; i < int.MaxValue; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 j++;
 
